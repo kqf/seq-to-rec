80189225b40939f4baa0ad8dde8f4afd5162a49e
Sasha <q.dev.null@gmail.com>
2020-09-21 07:46:18 +0300

    Wrap the POP method into a sklearn estimator

--- model/pop.py
+++ model/pop.py
@@ -1,6 +1,9 @@
 import click
 import pathlib
 import pandas as pd
+import itertools
+
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
 def read_data(raw):
@@ -12,15 +15,27 @@ def read_data(raw):
     )
 
 
+class PopEstimator(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.popualar_ = None
+
+    def fit(self, X, y=None):
+        exploded = X.str.split().explode("text")
+        freq = exploded.groupby(exploded).size()
+        self.popualar_ = freq.sort_values(ascending=False)[:20]
+        return self
+
+    def predict(self, X):
+        return itertools.repeat(self.popualar_, len(X))
+
+
 @click.command()
 @click.option(
     "--path", type=click.Path(exists=True), default="data/processed/")
 def main(path):
     train, test, valid = read_data(path)
-    exploded = train.str.split().explode("text")
-    freq = exploded.groupby(exploded).size()
-    most_popular = freq.sort_values(ascending=False)[:20]
-    print(most_popular)
+    model = PopEstimator().fit(train)
+    print(model.predict(train))
 
 
 if __name__ == '__main__':
