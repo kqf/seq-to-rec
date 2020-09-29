d023101d11ef25e2541754a45cd318658392f12c
Sasha <q.dev.null@gmail.com>
2020-09-29 07:29:46 +0300

    Remove the duplicated code from flat models

--- model/flat.py
+++ model/flat.py
@@ -9,8 +9,9 @@ import numpy as np
 import pandas as pd
 
 from sklearn.pipeline import make_pipeline
-from sklearn.base import BaseEstimator, TransformerMixin
-from torchtext.data import Example, Dataset, Field, BucketIterator
+from torchtext.data import Field, BucketIterator
+from torchtext.dataset import TextPreprocessor
+
 from irmetrics.topk import rr, recall
 
 from model.data import ev_data
@@ -30,25 +31,6 @@ def read_data(raw):
         pd.read_csv(path / 'test.txt', names=["text"]),
         pd.read_csv(path / 'valid.txt', names=["text"]),
     )
-
-
-class TextPreprocessor(BaseEstimator, TransformerMixin):
-    def __init__(self, fields, min_freq=1):
-        self.fields = fields
-        self.min_freq = min_freq
-
-    def fit(self, X, y=None):
-        dataset = self.transform(X, y)
-        for name, field in dataset.fields.items():
-            if field.use_vocab:
-                field.build_vocab(dataset, min_freq=self.min_freq)
-        return self
-
-    def transform(self, X, y=None):
-        with warnings.catch_warnings(record=True):
-            proc = [X[col].apply(f.preprocess) for col, f in self.fields]
-            examples = [Example.fromlist(f, self.fields) for f in zip(*proc)]
-            return Dataset(examples, self.fields)
 
 
 class DynamicVariablesSetter(skorch.callbacks.Callback):
