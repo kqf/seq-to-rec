ddc54f8b05bfbb5a668a45f226e18a8bfd483f24
Sasha <q.dev.null@gmail.com>
2020-10-06 08:22:22 +0300

    Increase the number of epochs

--- model/flat/mf.py
+++ model/flat/mf.py
@@ -55,7 +55,7 @@ def build_model(X_val=None, k=20):
         optimizer=torch.optim.Adam,
         optimizer__lr=0.002,
         criterion=torch.nn.CrossEntropyLoss,
-        max_epochs=1,
+        max_epochs=3,
         batch_size=128,
         iterator_train=SequenceIterator,
         iterator_train__shuffle=True,
