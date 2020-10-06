98f1608ec2b375648b3ab1c27715ed11817594dc
Sasha <q.dev.null@gmail.com>
2020-10-06 07:52:32 +0300

    Add test cases for OOV words in flat models

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -6,11 +6,17 @@ from model.flat.rnn import build_model as rnn
 from model.flat.mf import build_model as mf
 
 
-def test_data(flat_data, batch_size=32):
-    dataset = build_preprocessor().fit_transform(flat_data)
+def test_data(flat_data, flat_oov, batch_size=32):
+    model = build_preprocessor().fit(flat_data)
+    dataset = model.transform(flat_data)
     batch = next(iter(BucketIterator(dataset, batch_size=batch_size)))
     assert batch.text.shape[0] == batch_size
     assert batch.text.shape[1] == 4
+    oov = model.transform(flat_oov)
+
+    oov_batch = next(iter(BucketIterator(oov, batch_size=batch_size)))
+    assert oov_batch.text.shape == batch.text.shape
+    assert (oov_batch.text == 0).all()
 
 
 @pytest.mark.parametrize("build_model", [
