9984de1450df4183cf7d5577991f1bd34274f8ce
kqf <q.dev.null@gmail.com>
2020-10-23 07:58:13 +0200

    Add negative sampling to tests (#32)
    * Add negative sampling to tests
    
    * Add the rest of the missing modules to tests

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -6,7 +6,10 @@ from model.flat.srnn import build_model as srnn
 from model.flat.rnn import build_model as rnn
 from model.flat.mf import build_model as mf
 from model.flat.srgnn import build_model as srgnn
+from model.flat.quadratic import build_model as quadratic
 from model.flat.ns import build_model as ns
+# from model.flat.bpr import build_model as bpr
+from model.flat.bns import build_model as bns
 
 
 def test_data(flat_data, flat_oov, batch_size=32):
@@ -27,7 +30,10 @@ def test_data(flat_data, flat_oov, batch_size=32):
     mf,
     srnn,
     srgnn,
+    quadratic,
     ns,
+    # bpr,
+    bns,
 ])
 def test_model(build_model, flat_data):
     model = build_model(k=2).fit(flat_data)
